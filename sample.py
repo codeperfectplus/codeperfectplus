f7d05b29f62225b5c3dcfb43170e36954f573fb0
Deepak Raj <54245038+codePerfectPlus@users.noreply.github.com>
2022-10-28 10:07:28 +0530

    fixing
    Signed-off-by: Deepak Raj <54245038+codePerfectPlus@users.noreply.github.com>

--- sample.py
+++
@@ -1,9 +0,0 @@
-from subprocess import call
-
-date = '2022-07-21 10:00:00'
-
-call(['git', 'add', '.'], shell=True)
-
-call(['git',  'commit',  '--amend', '--date=2022-07-22 12:00:00'], shell=True)
-call(['git', 'pull'], shell=True)
-call(['git', 'push'], shell=True)
